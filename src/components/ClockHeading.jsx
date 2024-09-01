adefc11466deada4df9b5e297b999c0d6f9273b5
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2024-09-01 08:58:47 +0530

    This is Basic Clock Version

---
+++ src/components/ClockHeading.jsx
@@ -0,0 +1,5 @@
+let ClockHeading = () =>{
+  return <h1 className="fw-bolder">India Time</h1>;
+};
+
+export default ClockHeading;
---
+++ src/components/ClockSlogan.jsx
@@ -0,0 +1,5 @@
+let ClockSlogan = () =>{
+  return <p className="lead">This is the clock that shows time in Indian Standard Time(IST)</p>;
+};
+
+export default ClockSlogan;
