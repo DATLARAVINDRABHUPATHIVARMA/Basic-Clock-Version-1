309c40241e8b316b73657c97cac9a9fa3bfb3c02
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-01-15 22:14:17 -0600

    Clock Analysis

--- src/components/ClockSlogan.jsx
+++ src/components/ClockSlogan.jsx
@@ -1,14 +1,12 @@
-let ClockSlogan = () => {
+let ClockSlogan = () =>{
   let time = new Date();
   let timeZone = Intl.DateTimeFormat("en-US", {
-    timeZoneName: "long",
+    timeZoneName: "long"
   }).format(time);
 
   timeZone = timeZone.split(", ").pop();
 
-  return (
-    <p className="lead">Highness Sruthi's Empire is following {timeZone} </p>
-  );
+  return <p className="lead">Highness Sruthi's Empire is following {timeZone} </p>;
 };
 
 export default ClockSlogan;
