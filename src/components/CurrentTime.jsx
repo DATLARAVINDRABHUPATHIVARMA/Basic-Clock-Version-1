c727eb79e33eae953ac27f073d511d2beb9a3985
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-02-09 02:10:11 -0600

    Months Align

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -3,13 +3,13 @@ import React, { useEffect, useState } from "react";
 const CurrentTime = () => {
   const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
+  useEffect( () => {
     const timer = setInterval(() => {
       setTime(new Date());
     }, 1000);
 
     return () => clearInterval(timer);
-  }, []);
+  }, [] );
 
   const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
   const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
